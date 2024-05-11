892d9fcc4ad73c1aa7af479ada76657b2c126f4f
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-05-12 00:08:52 +0530

    added-side-barui

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,13 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { ITEMS, sidebarLinks } from '@/constants'
+import {  sidebarLinks } from '@/constants'
 import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
   return (
     <section className='sidebar'>
             <nav className='flex flex-col gap-4'>
@@ -19,9 +23,11 @@ const Sidebar = ({ user }: SiderbarProps) => {
                </Link>
 
                {sidebarLinks.map((item)=>{
+                const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+
                 return (
                   <Link href={item.route} key={item.label}
-                  className={cn('sidebar-link')}
+                  className={cn('sidebar-link',{'bg-bank-gradient':isActive})}
                   >
                     {item.label}
                   </Link>
